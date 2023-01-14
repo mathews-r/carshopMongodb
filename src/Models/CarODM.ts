bbe2192fb6ccce49c1dd380141347383efaba6a2
Mathews <mathewsrodrigues9@gmail.com>
2023-01-14 13:56:04 -0300

    adicionando tag para substituir _id por id

--- src/Models/CarODM.ts
+++ src/Models/CarODM.ts
@@ -4,15 +4,28 @@ import VehicleODM from './VehicleODM';
 
 export default class CarODM extends VehicleODM<ICar> {
   constructor() {
-    const schema = new Schema<ICar>({
-      model: { type: String, required: true },
-      year: { type: Number, required: true },
-      color: { type: String, required: true },
-      status: { type: Boolean, required: true },
-      buyValue: { type: Number, required: true },
-      doorsQty: { type: Number, required: true },
-      seatsQty: { type: Number, required: true },
-    });
+    const schema = new Schema<ICar>(
+      {
+        model: { type: String, required: true },
+        year: { type: Number, required: true },
+        color: { type: String, required: true },
+        status: { type: Boolean, required: true },
+        buyValue: { type: Number, required: true },
+        doorsQty: { type: Number, required: true },
+        seatsQty: { type: Number, required: true },
+      },
+      {
+        // https://www.mongodb.com/community/forums/t/how-to-rename-id-to-id/184632
+        id: true,
+        toJSON: {
+          transform(_doc, ret) {
+            const Ret = ret;
+            Ret.id = ret._id;
+            delete Ret._id;
+          },
+        },
+      },
+    );
     super(schema, 'Car');
   }
 }
