e790b5775130851d7cf4d8a1f940850211a7bf2e
Mathews <mathewsrodrigues9@gmail.com>
2023-01-14 11:21:07 -0300

    criando rota para get na cars

--- src/Routes/car.route.ts
+++ src/Routes/car.route.ts
@@ -4,5 +4,7 @@ import CarController from '../Controllers/CarController';
 const carRouter = express.Router();
 
 carRouter.post('/', (req, res, next) => new CarController(req, res, next).create());
+carRouter.get('/', (req, res, next) => new CarController(req, res, next).getAll());
+carRouter.get('/:id', (req, res, next) => new CarController(req, res, next).getById());
 
 export default carRouter;
