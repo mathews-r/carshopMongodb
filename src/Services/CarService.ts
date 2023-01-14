4ca9b009de70ab0f397d022740cf953e33c4a3ad
Mathews <mathewsrodrigues9@gmail.com>
2023-01-14 11:20:36 -0300

    criando service para getall e getid

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from 'mongoose';
 import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
@@ -14,5 +15,23 @@ export default class CarService {
     const carODM = new CarODM();
     const carCreated = await carODM.create({ ...car, status: car.status || false });
     return this.createCarDomain(carCreated);
+  };
+
+  public getAll = async () => {
+    const carODM = new CarODM();
+    const cars = await carODM.getAll();
+
+    return { status: 200, message: cars };
+  };
+
+  public getById = async (id: string) => {
+    if (!isValidObjectId(id)) return { status: 422, message: 'Invalid mongo id' };
+    const carODM = new CarODM();
+    const car = await carODM.getById(id);
+
+    if (!car) {
+      return { status: 404, message: 'Car not found' };
+    }
+    return { status: 200, message: car };
   };
 }
