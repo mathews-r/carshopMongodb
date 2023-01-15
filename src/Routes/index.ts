4f8fcb5862dc8fa2b7af57b10d49cf565a0c3e4a
Mathews <mathewsrodrigues9@gmail.com>
2023-01-15 20:52:28 -0300

    criando rota para moto

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import carRouter from './car.route';
+import motoRouter from './moto.route';
 
 const routes = express.Router();
 
 routes.use('/cars', carRouter);
+routes.use('/cars', motoRouter);
 
 export default routes;
---
+++ src/Routes/moto.route.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+// import MotorCycleController from '../Controllers/MotorCycle';
+
+const motoRouter = express.Router();
+
+// motoRouter.post('/', (req, res, next) => new MotorCycleController(req, res, next).create());
+// motoRouter.get('/', (req, res, next) => new MotorCycleController(req, res, next).getAll());
+// motoRouter.get('/:id', (req, res, next) => new MotorCycleController(req, res, next).getById());
+// motoRouter.put('/:id', (req, res, next) => new MotorCycleController(req, res, next).update());
+
+export default motoRouter;
