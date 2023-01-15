ffdc40edbdaa9328767355d2ef9c85cfc63e968a
Mathews <mathewsrodrigues9@gmail.com>
2023-01-15 20:48:24 -0300

    criando rota para update car

--- src/Routes/car.route.ts
+++ src/Routes/car.route.ts
@@ -6,5 +6,6 @@ const carRouter = express.Router();
 carRouter.post('/', (req, res, next) => new CarController(req, res, next).create());
 carRouter.get('/', (req, res, next) => new CarController(req, res, next).getAll());
 carRouter.get('/:id', (req, res, next) => new CarController(req, res, next).getById());
+carRouter.put('/:id', (req, res, next) => new CarController(req, res, next).update());
 
 export default carRouter;
