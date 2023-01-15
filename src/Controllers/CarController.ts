2441c2db00a03edbee696a3284ba04bc374c5e35
Mathews <mathewsrodrigues9@gmail.com>
2023-01-15 20:48:13 -0300

    criando funcao para update na controller car

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -54,4 +54,18 @@ export default class CarController {
       return this.res.status(422).json({ message: 'Invalid mongo id' });
     }
   };
+
+  public update = async () => {
+    const { id } = this.req.params;
+
+    const carChanged = this.req.body;
+
+    try {
+      const carUpdated = await this.service.updateCar(id, carChanged);
+      if (!carUpdated) return this.res.status(404).json({ message: 'Car not found' });
+      return this.res.status(200).json(carUpdated);
+    } catch (error) {
+      return this.res.status(422).json({ message: 'Invalid mongo id' });
+    }
+  };
 }
