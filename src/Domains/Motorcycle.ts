336ef8a0c0cc0448bd8db78b800cfa38fce9f69a
Mathews <mathewsrodrigues9@gmail.com>
2023-01-16 09:30:46 -0300

    refatorando Domains

---
+++ src/Domains/Motorcycle.ts
@@ -0,0 +1,29 @@
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import Vehicle from './Vehicle';
+
+export default class Motorcycle extends Vehicle {
+  private category: string;
+  private engineCapacity: number;
+
+  constructor(obj: IMotorcycle) {
+    super(obj);
+    this.category = obj.category;
+    this.engineCapacity = obj.engineCapacity;
+  }
+
+  public setCategory(category: string) {
+    this.category = category;
+  }
+
+  public getCategory() {
+    return this.category;
+  }
+
+  public setEngineCapacity(engineCapacity: number) {
+    this.engineCapacity = engineCapacity;
+  }
+
+  public getEngineCapacity() {
+    return this.engineCapacity;
+  }
+}
