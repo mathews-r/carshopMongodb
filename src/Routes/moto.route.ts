f30835840c68f94b6dc33134f047bb6bec7fa944
Mathews <mathewsrodrigues9@gmail.com>
2023-01-16 10:13:45 -0300

    criando rota para get motor

--- src/Routes/moto.route.ts
+++ src/Routes/moto.route.ts
@@ -4,8 +4,8 @@ import MotorCycleController from '../Controllers/MotorCycle';
 const motoRouter = express.Router();
 
 motoRouter.post('/', (req, res, next) => new MotorCycleController(req, res, next).create());
-// motoRouter.get('/', (req, res, next) => new MotorCycleController(req, res, next).getAll());
-// motoRouter.get('/:id', (req, res, next) => new MotorCycleController(req, res, next).getById());
+motoRouter.get('/', (req, res, next) => new MotorCycleController(req, res, next).getAll());
+motoRouter.get('/:id', (req, res, next) => new MotorCycleController(req, res, next).getById());
 // motoRouter.put('/:id', (req, res, next) => new MotorCycleController(req, res, next).update());
 
 export default motoRouter;
