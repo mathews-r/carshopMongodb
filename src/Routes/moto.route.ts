37936524bdcf414b376b274748eecc9cb573b149
Mathews <mathewsrodrigues9@gmail.com>
2023-01-16 10:53:53 -0300

    criando rota para deletar uma moto

--- src/Routes/moto.route.ts
+++ src/Routes/moto.route.ts
@@ -6,6 +6,7 @@ const motoRouter = express.Router();
 motoRouter.post('/', (req, res, next) => new MotorCycleController(req, res, next).create());
 motoRouter.get('/', (req, res, next) => new MotorCycleController(req, res, next).getAll());
 motoRouter.get('/:id', (req, res, next) => new MotorCycleController(req, res, next).getById());
-// motoRouter.put('/:id', (req, res, next) => new MotorCycleController(req, res, next).update());
+motoRouter.put('/:id', (req, res, next) => new MotorCycleController(req, res, next).update());
+motoRouter.delete('/:id', (req, res, next) => new MotorCycleController(req, res, next).delete());
 
 export default motoRouter;
