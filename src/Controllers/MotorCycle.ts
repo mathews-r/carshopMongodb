847db6a8d6ef7b574738cee43b4aa0556a00ab3b
Mathews <mathewsrodrigues9@gmail.com>
2023-01-16 10:53:40 -0300

    criando controller para deletar uma moto

--- src/Controllers/MotorCycle.ts
+++ src/Controllers/MotorCycle.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorCycle from '../Services/MotorCycle';
 
+const INVALID_MONGO_ID = 'Invalid mongo id';
+
 export default class MotorCycleController {
   private req: Request;
   private res: Response;
@@ -40,10 +42,41 @@ export default class MotorCycleController {
 
     try {
       const moto = await this.service.getById(id);
-      if (!moto) return this.res.status(404).json({ message: 'Motorcycle not found' });
+      if (!moto) {
+        return this.res.status(404).json({ message: 'Motorcycle not found' });
+      }
       return this.res.status(200).json(moto);
     } catch (error) {
-      return this.res.status(422).json({ message: 'Invalid mongo id' });
+      return this.res.status(422).json({ message: INVALID_MONGO_ID });
+    }
+  };
+
+  public update = async () => {
+    const { id } = this.req.params;
+
+    const motoChanged = this.req.body;
+
+    try {
+      const motoUpdated = await this.service.updateMoto(id, motoChanged);
+      if (!motoUpdated) {
+        return this.res.status(404).json({ message: 'Motorcycle not found' });
+      }
+      return this.res.status(200).json(motoUpdated);
+    } catch (error) {
+      return this.res.status(422).json({ message: INVALID_MONGO_ID });
+    }
+  };
+
+  public delete = async () => {
+    const { id } = this.req.params;
+    try {
+      const result = await this.service.deleteMoto(id);
+      if (result) {
+        return this.res.status(404).json(result);
+      }
+      return this.res.sendStatus(204);
+    } catch (error) {
+      return this.res.status(422).json({ message: INVALID_MONGO_ID });
     }
   };
 }
