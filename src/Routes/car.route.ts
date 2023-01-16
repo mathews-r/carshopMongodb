23972124281dec101790f2d138b3b50094243cb2
Mathews <mathewsrodrigues9@gmail.com>
2023-01-16 10:54:34 -0300

    criando rota para deletar carro

--- src/Routes/car.route.ts
+++ src/Routes/car.route.ts
@@ -7,5 +7,6 @@ carRouter.post('/', (req, res, next) => new CarController(req, res, next).create
 carRouter.get('/', (req, res, next) => new CarController(req, res, next).getAll());
 carRouter.get('/:id', (req, res, next) => new CarController(req, res, next).getById());
 carRouter.put('/:id', (req, res, next) => new CarController(req, res, next).update());
+carRouter.delete('/:id', (req, res, next) => new CarController(req, res, next).delete());
 
 export default carRouter;
